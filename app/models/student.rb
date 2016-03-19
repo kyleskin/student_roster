7d48de024bae0b9a9b8f5ce1d913f2562d91f79f
Kyle Skinner <kyleskin@gmail.com>
2016-03-18 19:12:31 -0600

    Validation
    * Created validations for student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ActiveRecord::Base
+  validates :first_name, :last_name, :email, :cell_phone, :home_phone, :work_phone, presence: true
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z]+\z/,
+    message: 'Only allows letters' }
+  validates :email, inclusion: { in: %w(@),
+            message: 'Not a valid email address' }
 end
